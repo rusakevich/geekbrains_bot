1d264ec31d0aa69b3bca48cbc7d605538691acb5
rusakevich <rusakevich.v@gmail.com>
2018-10-11 16:40:40 +0300

    Update bot2.py

--- teleg/bot2.py
+++ teleg/bot2.py
@@ -8,6 +8,7 @@ def remove_stickers():
     if os.path.exists('stickers.zip'):
         os.remove('stickers.zip')
 numbers_file = 1
+emoji = []
 TOKEN = '660998626:AAHgAzbMgxUhhLA8dlcb7u192Az5FCeWnpM'
 bot = telebot.TeleBot(TOKEN)
 BASE_URL = 'https://api.telegram.org/bot660998626:AAHgAzbMgxUhhLA8dlcb7u192Az5FCeWnpM/'
@@ -29,7 +30,7 @@ def start(message):
 def begin(message):
     remove_stickers()
     r = message.sticker
-    global numbers_file
+    global numbers_file, emoji
     param = {'file_id':r.file_id}
     fh = requests.post(BASE_URL+'getFile',data=param).json()['result']['file_path']
     sticker = urllib.request.urlopen(BASE_URL_FILE+fh).read()
@@ -37,23 +38,24 @@ def begin(message):
     out.write(sticker)
     out.close
     numbers_file += 1
-
+    emoji.append(r.emoji)
 
 
 @bot.message_handler(func=lambda message: message.text == "STOP")
 def send_something(message):
     try:
         remove_stickers()
-        global numbers_file
+        global numbers_file, emoji
         b=numbers_file-1
         while numbers_file-1:
-
             stickers_zip = zipfile2.ZipFile('stickers.zip', 'a')
             stickers_zip.write('sticker'+str(numbers_file-1)+'.webp', compress_type=zipfile2.ZIP_DEFLATED)
             stickers_zip.close()
             numbers_file -= 1
         doc = open('stickers.zip', 'rb')
         bot.send_document(message.chat.id, doc)
+        bot.send_message(message.chat.id, emoji if len(emoji)==1 else ' '.join(emoji))
+        emoji = []
         while b:
             os.remove('sticker'+str(b)+'.webp')
             b=b-1
